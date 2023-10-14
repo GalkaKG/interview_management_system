8cafee24e2a3f05bbcd0ea75bb8db5eeb48d566b
GalkaKG <galina_georgieva_net@abv.bg>
2023-10-14 17:50:51 +0300

    added Dockerfile, docker-compose and nginx configurations

--- interview_management_system/settings.py
+++ interview_management_system/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
+STATIC_ROOT = BASE_DIR / 'static_root'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
